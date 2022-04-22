a211e48b7734201df6d3b9d3fb8342d9f0f21f30
OliverDahlWagner <71690615+OliverDahlWagner@users.noreply.github.com>
2022-04-22 23:29:53 +0200

    add a comment
    comment in core pom

--- Core/src/main/java/etellerande.java
+++ Core/src/main/java/etellerande.java
@@ -9,11 +9,12 @@ public class etellerande {
     public static void main(String[] args) {
 
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
-
         //scan i package navn efter komponeter
         context.scan("");
         //skal man kalde
         context.refresh();
+
+
 
         for (Map.Entry<String, iConnect> iConnectEntry: context.getBeansOfType(iConnect.class).entrySet()) {
             System.out.println(iConnectEntry);
