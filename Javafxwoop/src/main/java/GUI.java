297b1e693d9abb553e6db6fb7b143d653b2493ee
Farhiya11 <ffarhiyaa@gmail.com>
2022-05-11 13:08:35 +0200

    h

--- Javafxwoop/src/main/java/GUI.java
+++ Javafxwoop/src/main/java/GUI.java
@@ -1,2 +1,31 @@
-public class GUI {
+import Controller.appController;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import javafx.application.Application;
+import static javafx.application.Application.launch;
+import javafx.stage.Stage;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+
+public class GUI extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/app.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setTitle("APP");
+        stage.setScene(scene);
+        stage.show();
+
+        //appControllerBean.init(stage);
+
+        //appControllerBean.loadScreen("/resources/app.fxml");
+
+
+    }
+
+
 }
