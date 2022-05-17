c351e211634f07e56fe94bff505685efd758a563
OliverDahlWagner <71690615+OliverDahlWagner@users.noreply.github.com>
2022-05-17 14:05:05 +0200

    updated the gui
    textareas and labels

--- CoreTemp/src/main/java/com/project/ChartController.java
+++ CoreTemp/src/main/java/com/project/ChartController.java
@@ -3,10 +3,43 @@ package com.project;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
 
 public class ChartController {
 
     Production production = new Production();
+
+    @FXML
+    private Label agvprint;
+
+    @FXML
+    private Label agvstatus;
+
+    @FXML
+    private Label assemblyprint;
+
+    @FXML
+    private Label assemblystatus;
+
+    @FXML
+    private TextArea inventoryprint;
+
+    @FXML
+    private TextArea processprint;
+
+    @FXML
+    private Label warehouseprint;
+
+    @FXML
+    private Label warehousestatus;
+
+    @FXML
+    void buttonclickedstop() {
+
+
+
+    }
 
     @FXML
     void buttonclicked() {
